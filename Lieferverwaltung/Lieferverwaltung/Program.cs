0607c7e2cd760759d9ae45fbd4f84e544d12b90c
M.Thomas <m.thomas@gso.schule.koeln>
2025-04-07 10:39:12 +0200

    Beispielobjekte werden erzeugt

--- Lieferverwaltung/Lieferverwaltung/Program.cs
+++ Lieferverwaltung/Lieferverwaltung/Program.cs
@@ -1,9 +1,33 @@
-﻿namespace Lieferverwaltung;
-
-class Program
+﻿namespace Lieferverwaltung
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello, World!");
+        static List<Lieferung> lieferungen = new List<Lieferung>();
+        static void Main(string[] args)
+        {
+            BeispielobjekteAnlegen();
+            Console.WriteLine(lieferungen.Count);
+        }
+
+        static void BeispielobjekteAnlegen()
+        {
+            lieferungen.Add(new Lieferung(
+                new DateOnly(2024, 06,22)
+                , "HHX05NNW0ZP"
+                , "86309"
+            ));
+            
+            lieferungen.Add(new Lieferung(
+                new DateOnly(2024, 09, 4)
+                , "GSV18EDC4BR"
+                , "91139"
+            ));
+            
+            lieferungen.Add(new Lieferung(
+                new DateOnly(2023, 04, 8)
+                , "CQX55KMY5RW"
+                , "27708"
+            ));
+        }
     }
 }
