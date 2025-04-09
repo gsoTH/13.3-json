218fd9e9dd0b2a79fed7d16af7dddc16eefe06fe
M.Thomas <m.thomas@gso.schule.koeln>
2025-04-09 12:25:23 +0200

    Dummy-Methode eingebaut. Fixes #1

--- Lieferverwaltung/Lieferverwaltung/Program.cs
+++ Lieferverwaltung/Lieferverwaltung/Program.cs
@@ -2,32 +2,42 @@
 {
     class Program
     {
-        static List<Lieferung> lieferungen = new List<Lieferung>();
+         
         static void Main(string[] args)
         {
-            BeispielobjekteAnlegen();
-            Console.WriteLine(lieferungen.Count);
+            List<Lieferung> lieferungen = new List<Lieferung>();
+            
+            BeispielobjekteAnlegen(lieferungen);
+            ErzeugeJSON(lieferungen);
         }
 
-        static void BeispielobjekteAnlegen()
+        static void BeispielobjekteAnlegen(List<Lieferung> originaleListe)
         {
-            lieferungen.Add(new Lieferung(
+            originaleListe.Add(new Lieferung(
                 new DateOnly(2024, 06,22)
                 , "HHX05NNW0ZP"
                 , "86309"
             ));
             
-            lieferungen.Add(new Lieferung(
+            originaleListe.Add(new Lieferung(
                 new DateOnly(2024, 09, 4)
                 , "GSV18EDC4BR"
                 , "91139"
             ));
             
-            lieferungen.Add(new Lieferung(
+            originaleListe.Add(new Lieferung(
                 new DateOnly(2023, 04, 8)
                 , "CQX55KMY5RW"
                 , "07708"
             ));
+        }
+
+        static void ErzeugeJSON(List<Lieferung> lieferungen)
+        {
+            throw new NotImplementedException();
+            // TODO: Datei erzeugen
+            // TODO: Anzahl der Objekte in Datei eintragen
+            // TODO: Alle Objekte in Datei auflisten
         }
     }
 }
